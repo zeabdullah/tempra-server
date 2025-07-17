b24821ed927ca710562d386dacfea08fae2db7dc
Abdullah Z <dev@zabd.dev>
2025-07-17 20:34:06 +0300

    init: first commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::group(['prefix' => 'auth'], function () {
+//     Route::get('/signup', function (Request $request) {
+//         return 'signup endpoint';
+//     });
+//     Route::get('/login', function (Request $request) {
+//         return 'login endpoint';
+//     });
+// });
